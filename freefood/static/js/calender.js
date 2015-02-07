98ee12f8edfcc1aa68373bff81b6271a2ee9718c
Rohan Varma <rnvarma@andrew.cmu.edu>
2015-02-07 07:58:47 -0500

    calender OP af

--- freefood/static/js/calender.js
+++ freefood/static/js/calender.js
@@ -1,12 +1,15 @@
-
-DATES = {0: "Sunday", 1: "Monday", 2: "Tuesday",
-         3: "Wednesday", 4: "Thursday", 5: "Friday",
-         6: "Saturday"}
+WEEKDIFF = 0;
 
 function resize_heights() {
   var w_size = $(window).height();
-  $(".sidebar").css("height", w_size);
-  $(".main-calender").css("height", w_size);
+  $(".sidebar").css("height", w_size - 50);
+  $(".main-calender").css("height", w_size - 51);
+
+  var cal_w = $(".main-calender").width();
+  $(".topdates").css("width", cal_w - 70);
+  $(".body-times").css("height", w_size - 100);
+  $(".body-stuff").css("height", w_size - 100);
+  $(".body-stuff").css("width", cal_w - 70);
 }
 
 function click_handlers() {
@@ -20,19 +23,87 @@ function click_handlers() {
       $(".sidebar-col").show();
       $(".main-col").addClass("col-md-10").addClass("col-sm-9").removeClass("col-md-12").removeClass("collapsed");
     }
+    resize_heights();
   });
+
+  $(".prev-week-button").click(function() {
+    var sunday = new Date();
+    WEEKDIFF -= 1
+    populate_top_dates(get_x_days_away(WEEKDIFF * 7));
+  });
+
+  $(".next-week-button").click(function() {
+    var sunday = new Date();
+    WEEKDIFF += 1
+    populate_top_dates(get_x_days_away(WEEKDIFF * 7));
+  });
+
+  $(".today-button-div").click(function() {
+    populate_top_dates(get_nearest_prev_sunday());
+  })
+}
+
+function get_prev_day(day) {
+  var next_day = moment(day)
+  next_day.subtract(1, "days");
+  return next_day;
+}
+
+function get_next_day(day) {
+  var next_day = moment(day)
+  next_day.add(1, "days");
+  return next_day;
+}
+
+function get_x_days_away(x) {
+  var sunday = get_nearest_prev_sunday();
+  for (var i = 0; i < Math.abs(x); i++) {
+    if (x < 0) {
+      sunday = get_prev_day(sunday);
+    } else {
+      sunday = get_next_day(sunday);
+    }
+  }
+  return sunday;  
 }
 
 function get_nearest_prev_sunday() {
-  var sunday = new Date();
+  var today = new Date();
+  var sunday = new moment();
   var day = today.getDay();
-  sunday.setDate(sunday.getDate() - day)
+  sunday.subtract(day, "days");
   return sunday
+}
+
+function get_days_of_week(sunday) {
+  var nextday;
+  nextday = moment(sunday);
+  days = [sunday];
+  for(var i = 1; i < 7; i++) {
+    nextday = get_next_day(nextday)
+    days.push(nextday)
+  }
+  return days;
+}
+
+function populate_top_dates(sunday) {
+  var days = get_days_of_week(sunday);
+  for (var i = 0; i < days.length; i++) {
+    var date = days[i];
+    var finaldate = date.format("dddd").slice(0,3) + " " + date.format("MM/DD");
+    $("#topdate" + i.toString()).text(finaldate);
+  }
+}
+
+function load_days() {
+  var sunday = get_nearest_prev_sunday();
+  populate_top_dates(sunday);
 }
 
 $(document).ready(function() {
   resize_heights();
   click_handlers();
-  var today = new Date();
-  console.log(today);
+  load_days();
 })
+
+
