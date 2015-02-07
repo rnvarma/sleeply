715c0a884ee260c09a6757ba7ad8a5292e278c15
Rohan Varma <rnvarma@andrew.cmu.edu>
2015-02-06 23:42:53 -0500

    pushed model migrations

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,5 @@ from django.conf import settings
 
 class UserData(models.Model):
   user = models.OneToOneField(settings.AUTH_USER_MODEL)
-  google_key = models.CharField(blank=True, null=True)
-  up_key = models.CharField(blank=True, null=True)
-  is_admin = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="is_admin")
+  google_key = models.CharField(blank=True, null=True, max_length=100)
+  up_key = models.CharField(blank=True, null=True, max_length=100)
