73d6590347f595552a97083b29b3d71b7b93cc5e
tiansjin <tiansjin@gmail.com>
2015-02-07 01:16:16 -0500

    javascript

---
+++ freefood/static/js/jawbone.js
@@ -0,0 +1,3 @@
+function connect(){
+	window.location.replace("https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=mAl_RHjkugQ&redirect_uri=https://localhost:8000/jawbone1&scope=basic_read extended_read location_read friends_read mood_read move_read sleep_read meal_read generic_event_read heartrate_read");
+}
